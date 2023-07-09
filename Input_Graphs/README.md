ec2550a6519dfe66fce1a2cad77f0d5151f08aee
Bibrak Qamar <bibrakc@gmail.com>
2023-07-09 12:23:36 +0500

    improved input generation

--- Input_Graphs/README.md
+++ Input_Graphs/README.md
@@ -1,14 +1,18 @@
 # Generate Synthetic Graphs Using NetworkX
-`$ python3 generate_input_graphs.py <Powerlaw|Scalefree|Smallworld|Erdos> <edge-factor> <scale-factor>`
+`$ python3 generate_input_graphs.py <Powerlaw|Scalefree|Smallworld|Erdos> <directed|not_directed> <edge-factor> <scale-factor>`
 
 Here:
 1. `[Powerlaw, Scalefree, Smallworld, Erdos]` are the graph types.
-2. `edge-factor` is the number of edges per vertex (approximately).
-3. `scale-factor` is the 2^n number of vertices, where n is the `scale-factor`.
+2. `directed` or `not_directed`:  If `directed` then use `directed=True` parameter whenever available.
+3. `edge-factor` is the number of edges per vertex (approximately).
+4. `scale-factor` is the 2^n number of vertices, where n is the `scale-factor`.
 
 ## Output
-The python script will create the `.edgelist` file containting the edges of the grath. The first two lines in the file contain the meta-data about the grapth, which includes the number of vertices and number of edges.
+The python script will create files: 
+1. `.edgelist`: containting the edges of the grath. The first two lines in the file contain the meta-data about the grapth, which includes the number of vertices and number of edges.
+2. `.output`: containing statistics about the generated graph.
+3. `.bfs`: containing breadth first search levels from source `0`.
+4. `.sssp`: containing single source shortest path lengths from source `0`.
+5. `.pagerank`: containing pagerank values.
 
-It will also output statistics about the graph in a `.output.txt` file.
-
-It will also generate plots of the statistics.
+`.bfs`, `.sssp`, and `.pagerank` can be used by applications for verification of results.
--- Input_Graphs/generate_input_graphs.py
+++ Input_Graphs/generate_input_graphs.py
@@ -30,6 +30,19 @@ def write_to_file(filename, content):
 # Output: find degrees and plot their distribution
 
 
+def In_Degree_Distribution(G):
+
+    degree = G.in_degree()
+    degree = [deg for (v, deg) in degree]
+    avg_degree = sum(degree)/len(degree)
+
+    print("In Degree Distribution Statistics")
+    s = pd.Series(degree)
+    print(s.describe())
+    write_to_file(Output_filename, "In Degree Distribution Statistics")
+    write_to_file(Output_filename, str(s.describe()))
+
+
 def Degree_Distribution(G):
     # start = time.time()
 
@@ -40,10 +53,10 @@ def Degree_Distribution(G):
     # end = time.time()
     # print("Avg Degree :", avg_degree, "\n")
 
-    plot_distribution(degree, xlabel='Degree ($k$)',
+    """ plot_distribution(degree, xlabel='Degree ($k$)',
                       ylabel='Number of nodes with degree $k$ ($N_k$)', title='Degree distributions '+A)
 
-    plt.savefig("Degree_Distribution_"+A+".png")
+    plt.savefig("Degree_Distribution_"+A+".png") """
 
     print("Degree Distribution Statistics")
     s = pd.Series(degree)
@@ -69,10 +82,10 @@ def CC_Distribution(G):
               "\nNumber of edges: ", m, "\n")
 
     cc_sizes = [len(c) for c in cc_sorted]
-    plot_distribution(cc_sizes, xlabel='Weakly connected component size',
+    """ plot_distribution(cc_sizes, xlabel='Weakly connected component size',
                       ylabel='Count', title='Connected component size distributions '+A)
 
-    plt.savefig("CC_Distribution_"+A+".png")
+    plt.savefig("CC_Distribution_"+A+".png") """
 
 
 # Input: A graph
@@ -110,10 +123,10 @@ def Clustering_Analysis(G):
 
     # print("Average clustering coefficient: ", avg_clust_coefficient,"\n")
     # plot the distribution of clustering coefficient
-    plot_distribution(local_clust_coefficient, xlabel='Clustering coefficient',
+    """ plot_distribution(local_clust_coefficient, xlabel='Clustering coefficient',
                       ylabel='Number of vertices', title='Clustering coefficient distributions '+A,
                       xlog=False, ylog=True, showLine=False)
-    plt.savefig("Clustering_Analysis_"+A+".png")
+    plt.savefig("Clustering_Analysis_"+A+".png") """
 
     print("Clustering Statistics")
     s = pd.Series(local_clust_coefficient)
@@ -155,11 +168,11 @@ def ShortestPaths_Analysis(G):
         avg_shortest_path_lens = sum(
             shortest_path_lens)/len(shortest_path_lens)
         # print("Average shortest_path_lens: ", avg_shortest_path_lens)
-        plot_distribution(shortest_path_lens, xlabel='Shortest path lengths (hops)',
+        """ plot_distribution(shortest_path_lens, xlabel='Shortest path lengths (hops)',
                           ylabel='Number of paths', title='Shortest path lengths distributions '+A,
                           xlog=False, ylog=False, showLine=True, intAxis=True)
 
-        plt.savefig("ShortestPaths_Analysis"+A+".png")
+        plt.savefig("ShortestPaths_Analysis"+A+".png") """
 
         print("Shortest Path Lengths Statistics")
         s = pd.Series(shortest_path_lens)
@@ -221,7 +234,7 @@ def plot_degree_bar(G):
 
 
 # Perform BFS using nx.bfs_tree
-def bfs(graph, source, target_node_for_printing, Output_filename):
+def bfs(graph, source, Output_filename):
     bfs_tree = nx.bfs_tree(graph, source)
     level = {source: 0}  # Dictionary to store the level of each node
     # Queue to keep track of nodes and their levels
@@ -229,18 +242,46 @@ def bfs(graph, source, target_node_for_printing, Output_filename):
 
     while queue:
         node, node_level = queue.popleft()
-        if node == target_node_for_printing:
-            print("\nBFS level")
-            print(f"Node: {node}, Level: {node_level}")
-            write_to_file(Output_filename, "\nBFS level")
-            write_to_file(Output_filename, "Node: " +
-                          str(node)+", Level: "+str(node_level))
-            break
-
         for neighbor in bfs_tree.neighbors(node):
             if neighbor not in level:
                 level[neighbor] = node_level + 1
                 queue.append((neighbor, node_level + 1))
+
+    # Open a file for writing the SSSP values
+    with open(Output_filename, "w") as file:
+        file.write(f"# Source\n")
+        file.write(f"{source}\n")
+        # Write the BFS values to the file
+        for vertex, level in level.items():
+            file.write(f"{vertex}\t{level}\n")
+
+
+# Perform SSSP using nx.single_source_dijkstra
+def sssp(graph, source, Output_filename):
+    sssp = nx.single_source_dijkstra(graph, source=source)
+    write_to_file(Output_filename, "# Source")
+    write_to_file(Output_filename, str(source))
+
+    # Open a file for writing the SSSP values
+    with open(Output_filename, "w") as file:
+        file.write(f"# Source\n")
+        file.write(f"{source}\n")
+        # Write the SSSP values to the file
+        for vertex, length in sssp[0].items():
+            file.write(f"{vertex}\t{length}\n")
+
+# Perform pagerank using nx.pagerank
+
+
+def pagerank(graph, Output_filename):
+    # Calculate PageRank
+    pagerank = nx.pagerank(graph, max_iter=100)
+
+    # Open a file for writing the PageRank values
+    with open(Output_filename, "w") as file:
+        # Write the PageRank values to the file
+        for vertex, pagerank_value in pagerank.items():
+            file.write(f"{vertex}\t{pagerank_value}\n")
 
 
 # Main
@@ -248,33 +289,34 @@ args = sys.argv
 
 # graph_types = ["Powerlaw", "Scalefree", "Smallworld", "Erdos"]
 graph = args[1]
+# Use directed=True parameter whenever available. directed or not_directed
+directed = args[2]
 
-edge_factor = int(args[2])
-scale_factor = int(args[3])
+edge_factor = int(args[3])
+scale_factor = int(args[4])
 vertices_needed = 2**scale_factor
 edges_needed = vertices_needed * edge_factor
 
 
 print(graph)
-Output_filename = graph+"_ef_" + \
-    str(edge_factor)+"_v_"+str(scale_factor)+"_output.txt"
-write_to_file(Output_filename, graph+"\n")
+Output_filename = ""
 
-if graph == "Powerlaw":
+
+if graph == "Powerlaw" and directed == "not_directed":
     G_gen = nx.powerlaw_cluster_graph(vertices_needed, edge_factor, .4)
     while (nx.is_connected(G_gen) == False):
         print(graph+" was not connected trying again")
         G_gen = nx.powerlaw_cluster_graph(vertices_needed, edge_factor, .4)
     A = "Powerlaw-clustered_ef_"+str(edge_factor)+"_v_"+str(scale_factor)
 
-if graph == "Scalefree":
+if graph == "Scalefree" and directed == "not_directed":
     G_gen = nx.barabasi_albert_graph(vertices_needed, edge_factor)
     while (nx.is_connected(G_gen) == False):
         print(graph+" was not connected trying again")
         G_gen = nx.barabasi_albert_graph(vertices_needed, edge_factor)
     A = "Scale-free_ef_"+str(edge_factor)+"_v_"+str(scale_factor)
 
-if graph == "Smallworld":
+if graph == "Smallworld" and directed == "not_directed":
     G_gen = nx.watts_strogatz_graph(vertices_needed, edge_factor, .2)
     while (nx.is_connected(G_gen) == False):
         print(graph+" was not connected trying again")
@@ -282,14 +324,23 @@ if graph == "Smallworld":
     A = "Small-world_ef_"+str(edge_factor)+"_v_"+str(scale_factor)
 
 
-if graph == "Erdos":
-    G_gen = nx.gnm_random_graph(vertices_needed, edges_needed, seed=133)
+if graph == "Erdos" and directed == "directed":
+    G_gen = nx.gnm_random_graph(
+        vertices_needed, edges_needed, directed=True, seed=133)
+    A = "Erdos-Renyi_directed_ef_"+str(edge_factor)+"_v_"+str(scale_factor)
+
+if graph == "Erdos" and directed == "not_directed":
+    G_gen = nx.gnm_random_graph(
+        vertices_needed, edges_needed, seed=133)
     while (nx.is_connected(G_gen) == False):
         print(graph+" was not connected trying again")
-        G_gen = nx.gnm_random_graph(vertices_needed, edges_needed, seed=133)
+        G_gen = nx.gnm_random_graph(
+            vertices_needed, edges_needed, directed=True, seed=133)
     A = "Erdos-Renyi_ef_"+str(edge_factor)+"_v_"+str(scale_factor)
 
 
+Output_filename = A+".output"
+write_to_file(Output_filename, graph+"\n")
 # print(A, ": Number of vertices:", G_gen.number_of_nodes(), ", Number of edges: ", G_gen.number_of_edges())
 # write_to_file(Output_filename, A+ ": Number of vertices:"+ str(G_gen.number_of_nodes())+ ", Number of edges: "+ str(G_gen.number_of_edges()))
 
@@ -301,12 +352,19 @@ print("Graph generated with weights\n")
 # Analyze the graph that you have created
 
 # Degree Distribution is time consuming
+if directed != "not_directed":
+    start = time.time()
+    In_Degree_Distribution(G_gen)
+    end = time.time()
+    print("Time in In_Degree_Distribution: ", end-start, "\n")
+
 start = time.time()
 Degree_Distribution(G_gen)
 end = time.time()
 print("Time in Degree_Distribution: ", end-start, "\n")
 
 
+# These are time consuming therefore commenting them out.
 """ start = time.time()
 Clustering_Analysis(G_gen)
 end = time.time()
@@ -319,15 +377,23 @@ end = time.time()
 print("Time in SSSP: ", end-start, "\n") """
 
 
-length, path = nx.single_source_dijkstra(G_gen, 0, 35, weight='weight')
+filename_to_write = A+".edgelist"
+
+""" length, path = nx.single_source_dijkstra(G_gen, 0, 35, weight='weight')
 print("\nSSSP path length with wieghts from Src: 0 to target: 35 = ", length)
 print("SSSP path = ", path)
 write_to_file(Output_filename,
               "\nSSSP path length with wieghts from Src: 0 to target: 35 = " + str(length))
-write_to_file(Output_filename, "SSSP path = "+str(path))
+write_to_file(Output_filename, "SSSP path = "+str(path)) """
 
-# Call BFS starting from node 0. Print the level of target node 35
-bfs(G_gen, 0, 35, Output_filename)
+# Call BFS starting from node 0. Write the levels in a file.
+bfs(G_gen, 0, filename_to_write+".bfs")
+
+# Call SSSP starting from node 0. Write the levels in a file.
+sssp(G_gen, 0, filename_to_write+".sssp")
+
+# Perform pagerank using nx.pagerank. Write the values in a file.
+pagerank(G_gen, filename_to_write+".pagerank")
 
 # sssp = nx.shortest_path(G_gen,source=4, target=47, weight=None)#, weight='weight')
 # sssp = nx.shortest_path(G_gen,source=0, target=35,  weight='weight')
@@ -356,16 +422,17 @@ end = time.time()
 print("Time in CC: ", end-start, "\n")
 '''
 
-# Store the graph as directed graph in file
-G = G_gen.to_directed()
-
+# For non directed graphs first convert them to directed. Then store both edges.
+if directed != "directed":
+    # Store the graph as directed graph in file
+    G = G_gen.to_directed()
+else:
+    G = G_gen
 
 # nx.draw(G, with_labels=True)
 # Show the graph
 # plt.show()
 
-
-filename_to_write = A+".edgelist"
 
 nx.write_weighted_edgelist(G, filename_to_write, delimiter='\t')
 
